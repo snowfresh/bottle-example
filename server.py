a0145c5e551b82e8801b6928ea5fb804ab11ba04
snowfresh <irkyle@comcast.net>
2016-02-29 14:23:59 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 import bottle
+import random
 
 APP = bottle.default_app()
 
 @APP.route('/')
 def index():
   return '<p>Hello</p>'
+  
+  @APP.route('/random')
+  def random():
+    return random.randint(0, 10)
 
 if __name__ == '__main__':
   bottle.run(application=APP)
